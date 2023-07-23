756504e337ccd6cb4f3992037e4265ee4bff8d6c
FrankFontcha <frank.fontcha@vaimanagement.co>
2023-07-23 10:45:10 +0400

    update layout

--- project/resources/views/home2.blade.php
+++ project/resources/views/home2.blade.php
@@ -5,12 +5,12 @@
 <section class="content">
     <div class="container-fluid">
         <!-- Small boxes (Stat box) -->
-        <div class="row">
-            <h3>Bienvenu sur votre espace de travail</h3>
+        <div class="row text-center pt-5">
+            <h3>Bienvenue sur votre espace de travail</h3>
         </div>
 
-        <div class="row">
-            <img src="{{asset('img/logo_rita.jpeg')}}" alt="logo" style="width: 100%">
+        <div class="container p-4 position-relative border-radius" style="max-width: 300px;">
+            <img src="{{asset('img/logo_rita.jpeg')}}" alt="logo" style="width: 100%; border-radius:50%;">
         </div>
 
     </div><!-- /.container-fluid -->
